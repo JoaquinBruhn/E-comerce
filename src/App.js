9a28b71edb5784fb044d3d323dc41591ffc301e4
Agus Brollo <brolloagus@gmail.com>
2022-03-11 16:15:19 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import Pirulo from "./components/pirulo";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <h3>Pirulo</h3>
-      <Pirulo />
-      <Pirulo />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h3>Pirulo</h3>
+			<Pirulo />
+			<Pirulo />
+		</div>
+	)
 }
 
-export default App;
+export default App
