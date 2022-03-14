eb43a67b87de0fd77b88ca43b80e70b89000a6d3
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-03-14 18:10:06 -0300

    added some basic routes as a guide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { Home } from "./pages/home.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/navbar.js";
+import { Home } from "./pages/home.js";
+import Category from "./pages/category.js";
+import NotFound from "./pages/404.js";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./global-styles.css";
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="category/:category" element={<Category />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
