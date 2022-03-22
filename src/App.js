439f47d6bd6864e17e8e281d1bb444400d0d2db7
Joaquin <joaquin_bruhn@gmail.com.ar>
2022-03-21 22:45:39 -0300

    added some more routes to be linked to the navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Navbar } from "./components/navbar/navbar.js";
 import { Home } from "./pages/home.js";
+import Cart from "./pages/cart.js";
+import BoughtList from "./pages/boughtList.js";
+import Login from "./pages/login.js";
 import Category from "./pages/category.js";
 import NotFound from "./pages/404.js";
 
@@ -13,6 +16,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/boughtlist" element={<BoughtList />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/login" element={<Login />} />
           <Route exact path="category/:category" element={<Category />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+
+  return (
+    <>
+      <h3>This are the login page</h3>
+    </>
+  );
+};
+
+export default Login;
